9224a54eeac3f4641df7060a2e23a98f71e06308
bailey <111161049+byoung22@users.noreply.github.com>
2023-11-16 12:16:12 -0800

    Slideshow created

--- src/components/Slideshow/Slideshow.tsx
+++ src/components/Slideshow/Slideshow.tsx
@@ -1,31 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import 
 
 type Props = {
-  results: string[],
+  results: Game[],
+};
+
+type Game = {
+  name: string,
+  short_screenshots: GameScreenshots[],
+};
+
+type GameScreenshots = {
+  image: string,
 };
 
 export default function Slideshow({ results }: Props) {
   const [selection, setSelection] = useState(0);
-  const [mainPhoto, setMainPhoto] = useState(results);
+  const [photos, setPhotos] = useState(results[0].short_screenshots);
+  useEffect(() => {
+
+  }, [selection]);
 
   return (
     <div>
+      <div><img src={photos[selection].image} alt={results[selection].name} /></div>
       <div>
-
-      </div>
-      <div>
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
+        <button type="button" aria-label={results[0].name} onClick={() => setSelection(0)} />
+        <button type="button" aria-label={results[1].name} onClick={() => setSelection(1)} />
+        <button type="button" aria-label={results[2].name} onClick={() => setSelection(2)} />
+        <button type="button" aria-label={results[3].name} onClick={() => setSelection(3)} />
+        <button type="button" aria-label={results[4].name} onClick={() => setSelection(4)} />
+        <button type="button" aria-label={results[5].name} onClick={() => setSelection(5)} />
+        <button type="button" aria-label={results[6].name} onClick={() => setSelection(6)} />
+        <button type="button" aria-label={results[7].name} onClick={() => setSelection(7)} />
+        <button type="button" aria-label={results[8].name} onClick={() => setSelection(8)} />
+        <button type="button" aria-label={results[9].name} onClick={() => setSelection(9)} />
+        <button type="button" aria-label={results[10].name} onClick={() => setSelection(10)} />
+        <button type="button" aria-label={results[11].name} onClick={() => setSelection(11)} />
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,25 +5,25 @@ import key from '../key.js';
 import data from './data.js';
 
 type Games = {
-  results: string[];
+  results: object[];
   next: string;
 };
 
-function threeMonthInterval() {
-  const today = new Date();
-  const month = String(today.getMonth() + 1).padStart(2, '0');
-  const year = today.getFullYear();
-  let prevMonth: number | string;
-  let prevYear: number | string;
-  if (parseInt(month, 10) > 3) {
-    prevMonth = String(today.getMonth() - 2).padStart(2, '0');
-    prevYear = year;
-  } else {
-    prevMonth = String(today.getMonth() + 10).padStart(2, '0');
-    prevYear = year - 1;
-  }
-  return `${prevYear}-${prevMonth}-01,${year}-${month}-01`;
-}
+// function threeMonthInterval() {
+//   const today = new Date();
+//   const month = String(today.getMonth() + 1).padStart(2, '0');
+//   const year = today.getFullYear();
+//   let prevMonth: number | string;
+//   let prevYear: number | string;
+//   if (parseInt(month, 10) > 3) {
+//     prevMonth = String(today.getMonth() - 2).padStart(2, '0');
+//     prevYear = year;
+//   } else {
+//     prevMonth = String(today.getMonth() + 10).padStart(2, '0');
+//     prevYear = year - 1;
+//   }
+//   return `${prevYear}-${prevMonth}-01,${year}-${month}-01`;
+// }
 
 function Home() {
   const [games, setGames] = useState<Games | null>(null);
@@ -31,9 +31,9 @@ function Home() {
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    setGames(data)
-    console.log(data)
-  }, [])
+    setGames(data);
+    console.log(data);
+  }, []);
   // useEffect(() => {
   //   const getGames = async () => {
   //     try {
