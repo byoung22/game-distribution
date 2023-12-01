336eb2f9919c8d303027544b25f74d4f9cea7125
Bailey <111161049+byoung22@users.noreply.github.com>
2023-11-30 17:09:26 -0800

    Scroller cahnged

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,15 +1,27 @@
 import { useState } from 'react';
 import { InfoType } from '../../types/types';
-import Slider from '../Slider/Slider';
 import styles from './GameCard.module.css';
 
 export default function GameCard({ gameData, descriptionData }: InfoType) {
-  const [displayIndex, setdisplayIndex] = useState(0);
+  const [displayIndex, setDisplayIndex] = useState(0);
 
   const game = gameData.results[0];
   const mainImage = gameData.results[0].short_screenshots[0].image;
   const screenshots = gameData.results[0].short_screenshots.slice(1);
   const firstParagraph = descriptionData.description_raw.split('\n\n')[0];
+
+  // RECENT REVIEWS:
+  // Mostly Positive (14,977)
+  // ALL REVIEWS:
+  // Mostly Positive (426,401) *
+  // RELEASE DATE:
+  // Aug 15, 2013
+  // DEVELOPER:
+  // Gaijin Entertainment
+  // PUBLISHER:
+  // Gaijin Distribution KFT
+  // Popular user-defined tags for this product:
+  // Free to PlayVehicular CombatCombatVR+
 
   return (
     <div className={styles.card}>
@@ -18,12 +30,16 @@ export default function GameCard({ gameData, descriptionData }: InfoType) {
       </div>
       <div className={styles.screenshotBox}>
         <div className={styles.screenshotList}>
-          {screenshots.map((obj) => (
-            <div className={styles.screenshotCard} key={obj.id}>
+          {screenshots.map((obj, index) => (
+            <button
+              type="button"
+              className={styles.screenshotCard}
+              key={obj.id}
+              onClick={() => setDisplayIndex(index)}
+            >
               <img className={styles.image} src={obj.image} alt={game.name} />
-            </div>
+            </button>
           ))}
-          <input type="range" min="1" max="100" value="50" className="slider" id="myRange" />
         </div>
       </div>
       <div />
